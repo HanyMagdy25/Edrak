5079f9c75c007975c12336c187f3cbc61c22838d
HanyMagdy25 <hany.magdy25@gmail.com>
2022-04-18 14:31:08 +0200

    fix category

--- src/Cards/CardPodcast.js
+++ src/Cards/CardPodcast.js
@@ -13,7 +13,7 @@ const CardPodcast = ({data}) => {
           <div className='global-simi-btn-red'>هوية</div>
           <h2>مقاومة إمبريالية المعرفة</h2>
           <p>لأساطيرِ البدايةِ في العالم أجمع أثرٌ نفسِي جذري –بغض النظرِ عن مصداقيتها العلمية–؛ إذ لها القوة الماكرةِ على تبرير الوضع الراهن وبالتوازي تحصرُ وعيَ المرء بما يمكن أن يصير إليه شكلُ العالم المستقبل. وكذا حالُ المجتمع الرأسمالي الحديث الذي أسس نفسه على روايتين مختلفتين لأسطورة مماثلة.</p>
-          <div className='about-author'>
+          <div className='about-author-cardPodcast'>
                 <h6>أمجد عبد الرازق</h6>
                 <h5 className='share-icon-in-card'><i className="fa-solid fa-share-nodes"></i> 15</h5>
                 <h5 className='cal'><i className="fa-solid fa-calendar-days"></i> 22-2-2022</h5>
